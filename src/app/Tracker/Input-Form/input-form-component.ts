977d1c477910dc7dd38df78f02933a1d07527a05
metamorphic-rock <113041227+metamorphic-rock@users.noreply.github.com>
2023-01-20 09:55:53 +0800

    fix bug regarding negative values
    as input

--- src/app/Tracker/Input-Form/input-form-component.ts
+++ src/app/Tracker/Input-Form/input-form-component.ts
@@ -16,7 +16,7 @@ export class InputFormComponentComponent{
   };
   @Output() addSetEvent: EventEmitter<SetItem>=new EventEmitter<SetItem>(); //Change the Any to interface Type
   addSet=()=>{
-    if(this.exerciseSet.exercise_Name==""||this.exerciseSet.muscle_group==""||this.exerciseSet.reps==0){
+    if(this.exerciseSet.exercise_Name==""||this.exerciseSet.muscle_group==""||this.exerciseSet.reps==0||this.exerciseSet.weight<0){
       return;
     };
     console.log("Add set");
@@ -27,7 +27,7 @@ export class InputFormComponentComponent{
 
   @Output() addExerciseEvent: EventEmitter<any>=new EventEmitter<any>()
   finishExercise=()=>{
-    if(this.exerciseSet.exercise_Name==""||this.exerciseSet.muscle_group==""||this.exerciseSet.reps==0){
+    if(this.exerciseSet.exercise_Name==""||this.exerciseSet.muscle_group==""||this.exerciseSet.reps==0||this.exerciseSet.weight<0){
       return;
     };
     this.exerciseSet.exercise_Name='';
